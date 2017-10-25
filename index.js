441a1d08be770dd975320dcef0887f17f77d607b
Sanyam Suhas Mulay <sanyam@netdox.org>
2017-10-25 19:23:59 +0530

    added a package.json and index.js - untested

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Package Description:");
+console.log("YouTube PopUp is jQuery plugin to display YouTube or Vimeo in PopUp, compatible with WordPress, jQuery Plugin by Qassim Hassan – Owner of WP Time.");
